f3c849bab151fdaba5ac3988a54d2ccfd2debba1
John Robinson <J.Robinson@software.ac.uk>
2017-02-20 16:44:49 +0000

    Set up logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
+
+# Set up logging
+handler = RotatingFileHandler('saf.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s %(funcName)s() - %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+
+
 app.config['SECRET_KEY'] = 'change me'
 Bootstrap(app)
 
--- manage.py
+++ manage.py
@@ -1,4 +1,7 @@
 from app import app
 
+
 # Run this to start the webapp
+
 app.run(debug=True)
+app.logger.debug("Started up...")
